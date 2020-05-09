56ae1f2d20370fd9d94a0484f9276fcec8133140
= <=duongchihieu.090599@gmail.com>
2020-05-09 10:06:38 +0700

    commit Telnet

---
+++ ClientServerTelnet/Client/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Client
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TcpClient tcpClient = new TcpClient();
+            tcpClient.Connect("192.168.1.7",5000);
+            Console.WriteLine("Connect Successful");
+            Stream stream = tcpClient.GetStream();
+            ASCIIEncoding encoding = new ASCIIEncoding();
+            //send
+            byte[] data = encoding.GetBytes(Dns.GetHostByName(Dns.GetHostName()).AddressList[0].ToString());
+            stream.Write(data, 0, data.Length);
+            data = new byte[1024];
+            //receive
+            stream.Read(data, 0, 1024);
+            Console.WriteLine(encoding.GetString(data));
+            Console.ReadKey();
+        }
+    }
+}
---
+++ ClientServerTelnet/Server/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Sockets;
+
+namespace Server
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IPAddress ipLocal = IPAddress.Parse("192.168.1.7");
+            TcpListener tcpListener = new TcpListener(ipLocal, 5000);
+            tcpListener.Start();
+            Console.WriteLine("Server Start In :" + tcpListener.LocalEndpoint);
+            Console.WriteLine("Waiting the Connect...");
+            while (true)
+            {
+                Socket socket = tcpListener.AcceptSocket();
+                Console.WriteLine(socket.RemoteEndPoint + " connected");
+                byte[] data = new byte[1024];
+                socket.Receive(data);
+                ASCIIEncoding encoding = new ASCIIEncoding();
+                string str = encoding.GetString(data);
+                socket.Send(encoding.GetBytes("Hello" + str));
+            }
+            
+
+        }
+    }
+}
