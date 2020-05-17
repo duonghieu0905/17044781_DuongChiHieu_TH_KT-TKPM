51d15b67cb158cdbbf0187fa1a4e9d8b60cd80e3
= <=duongchihieu.090599@gmail.com>
2020-05-17 08:02:56 +0700

    tuan 2 active mq

---
+++ ActiveMQ/ActiveMQReicever/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Apache.NMS;
+using System.Threading.Tasks;
+
+using Apache.NMS.ActiveMQ;
+using Apache.NMS.ActiveMQ.Commands;
+
+namespace ActiveMQReicever
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IConnectionFactory factory =new  ConnectionFactory("tcp://localhost:61616");
+            IConnection connection = factory.CreateConnection("admin", "admin");
+            connection.Start();
+            ISession session = connection.CreateSession(AcknowledgementMode.AutoAcknowledge);
+            ActiveMQQueue queue = new ActiveMQQueue("17044781_DuongChiHieu");
+            IMessageConsumer consumer = session.CreateConsumer(queue);
+            consumer.Listener += Consumer_Listener;
+            while (true)
+            {
+             
+            }
+
+            
+        }
+
+        private static void Consumer_Listener(IMessage message)
+        {
+            if(message is ActiveMQTextMessage)
+            {
+                Console.WriteLine(((ActiveMQTextMessage)message).Text);
+            }
+           
+        }
+    }
+}
---
+++ ActiveMQ/ActiveMQSender/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Apache.NMS;
+using Apache.NMS.ActiveMQ;
+using Apache.NMS.ActiveMQ.Commands;
+
+namespace ActiveMQSender
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IConnectionFactory factory = new ConnectionFactory("tcp://localhost:61616");
+            IConnection connection = factory.CreateConnection("admin", "admin");
+            connection.Start();
+            ISession session = connection.CreateSession(AcknowledgementMode.AutoAcknowledge);
+            IMessageProducer producer = session.CreateProducer(new ActiveMQQueue("17044781_DuongChiHieu"));
+            while (true)
+            {
+                Console.WriteLine("Nhap thong tin");
+                string s = Console.ReadLine();
+                producer.Send(producer.CreateTextMessage(s));
+            }
+        }
+    }
+}
